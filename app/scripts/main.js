0fee6e07fa8e8b71efdbc9d2427fb005c5fe2f58
rcrespo9 <rudy.crespo20@gmail.com>
2017-07-05 14:22:51 -0400

    added map, styles and script template

---
+++ app/scripts/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+(function() {
+	function init() {
+	}
+
+	function ready(fn) {
+	  if (document.readyState != 'loading'){
+	    fn();
+	  } else {
+	    document.addEventListener('DOMContentLoaded', fn);
+	  }
+	}
+
+	ready(init);
+})();
