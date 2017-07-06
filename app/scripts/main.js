ddabc8810de0d841ae8b84ca9218f1d2e1aecb0d
rcrespo9 <rudy.crespo20@gmail.com>
2017-07-06 09:38:54 -0400

    highlight country on map, optimized svg, organized js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,38 @@
 'use strict';
 
 (function() {
+	const detectLocation = () => {
+		const $worldMap = document.querySelector('#js-map');
+
+		function detectUserIp() {
+			fetch('//freegeoip.net/json/').then(function(response) {
+				if(response.ok) {
+					return response.json();
+				}
+				throw new Error('Network response was not okay.');
+			}).then(function(data) {
+				const {country_code, country_name} = data;
+				const $country = $worldMap.querySelector(`#${country_code}`);
+
+				console.log(data);
+
+				$country.classList.add('active-country');
+			}).catch(function(error) {
+				console.log('There has been a problem with your fetch operation: ' + error.message);
+			});
+		}
+
+		return {
+			init: () => {
+				detectUserIp();
+			}
+		};
+	};
+
 	function init() {
-		fetch('//freegeoip.net/json/').then(function(response) {
-			return response.json();
-		}).then(function(data) {
-			console.log(data); 
-		});
+		const newDetectLoc = detectLocation();
+
+		newDetectLoc.init();
 	}
 
 	function ready(fn) {
